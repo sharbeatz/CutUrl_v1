d1d54b9b2535bda015be4fd4291a481f2deaee4f
sharbeatz <164811194+sharbeatz@users.noreply.github.com>
2024-07-07 08:13:41 +0300

    Update models.py

--- CutUrl1-1.3/url_shortener/shortener/models.py
+++ CutUrl1-1.3/url_shortener/shortener/models.py
@@ -2,14 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class URL(models.Model):
-    original_url = models.URLField()
-    short_code = models.CharField(max_length=10, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    original_url = models.URLField('Полный URL-адрес')
+    short_code = models.CharField('Короткий код', max_length=6, unique=True)
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
 
     def __str__(self):
         return self.original_url
-
-
 
